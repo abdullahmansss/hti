6462ec6470b6b728c53d8d122eea27da67abaefe
Abdullah Mansour <abdullah.mansour97@gmail.com>
2020-09-04 13:05:57 +0200

    bottom nav v2

--- app/src/main/java/mans/firstapp/Tab1.java
+++ app/src/main/java/mans/firstapp/Tab1.java
@@ -1,5 +1,7 @@
 package mans.firstapp;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,16 +9,82 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Tab1 extends Fragment
 {
     View view;
+    FloatingActionButton floatingActionButton;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_tab1, container, false);
         return  view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+
+        initViews();
+    }
+
+    private void initViews()
+    {
+        floatingActionButton = view.findViewById(R.id.dialog_fab);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                showDialog();
+            }
+        });
+    }
+
+    public static class userDialog extends DialogFragment
+    {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState)
+        {
+            // Use the Builder class for convenient dialog construction
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            // Get the layout inflater
+            LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+            // Inflate and set the layout for the dialog
+            // Pass null as the parent view because its going in the dialog layout
+            builder.setView(inflater.inflate(R.layout.user_dialog, null))
+                    // Add action buttons
+                    .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            // sign in the user ...
+                            dialog.cancel();
+
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id)
+                        {
+                            dialog.cancel();
+                        }
+                    });
+
+            return builder.create();
+        }
+    }
+
+    public void showDialog()
+    {
+        DialogFragment newFragment = new userDialog();
+        newFragment.show(getActivity().getSupportFragmentManager(), "users");
     }
 }
